5741d9b4669820292a4f473c1b00b43efb603138
Giulia Ramos <84431836+giuliarm@users.noreply.github.com>
2024-04-06 13:26:06 -0300

    Remoção arquivos de  SSR não utilizado.

--- src/main.server.ts
+++
@@ -1,7 +0,0 @@
-import { bootstrapApplication } from '@angular/platform-browser';
-import { AppComponent } from './app/app.component';
-
-
-const bootstrap = () => bootstrapApplication(AppComponent);
-
-export default bootstrap;
